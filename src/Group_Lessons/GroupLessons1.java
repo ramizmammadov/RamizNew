c325a7733fa1c7c050634acf85babddec7cde2fb
ramizmammadov <ramizmammadov71@gmail.com>
2023-10-17 11:52:45 -0500

    My first change

--- src/Group_Lessons/GroupLessons1.java
+++ src/Group_Lessons/GroupLessons1.java
@@ -3,7 +3,7 @@ package Group_Lessons;
 public class GroupLessons1 {
     public static void main(String[] args) {
         int [] num = {1,2,4,5,65};
-        //by Ramiz
+        //by Ramiz////lets see
     }
     }
 
