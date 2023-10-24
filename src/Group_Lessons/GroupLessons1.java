6d23c101d704fbd5bd52f6cad0514908f8ed4dcb
ramizmammadov <ramizmammadov71@gmail.com>
2023-10-24 10:44:31 -0500

    My first change

--- src/Group_Lessons/GroupLessons1.java
+++ src/Group_Lessons/GroupLessons1.java
@@ -1,9 +1,16 @@
 package Group_Lessons;
 
+import java.util.Scanner;
+
 public class GroupLessons1 {
     public static void main(String[] args) {
-        int [] num = {1,2,4,5,65};
-        //by Ramiz
+   int highest=-254532345;
+   int lowest =254532345;
+   int counter = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+
     }
     }
 
