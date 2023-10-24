1af89d1906b8e530d11459c89161dd0257e00fc6
ramizmammadov <ramizmammadov71@gmail.com>
2023-10-24 11:38:01 -0500

    My first change

---
+++ src/Group_Lessons/day25loops.java
@@ -0,0 +1,10 @@
+package Group_Lessons;
+
+public class day25loops {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
