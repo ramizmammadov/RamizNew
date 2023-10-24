815d954fa3bc1c1d88c7ec25491d6d9e9d30e3fd
Dina <515lincolnstreet@gmail.com>
2023-10-24 12:18:39 -0400

    new update

--- src/Group_Lessons/GroupLessons1.java
+++ src/Group_Lessons/GroupLessons1.java
@@ -4,6 +4,7 @@ public class GroupLessons1 {
     public static void main(String[] args) {
         int [] num = {1,2,4,5,65};
         //by Ramiz////lets see
+        System.out.println("hello world");
     }
     }
 
